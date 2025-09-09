a9d8abe7908e79794725eeb6e62a670e13baa2da
adam moore <adamm@adams-MacBook-Air.local>
2025-09-08 20:25:30 -0500

    lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from "@eslint/js";
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+const js = require("@eslint/js");
+const typescript = require("@typescript-eslint/eslint-plugin");
+const typescriptParser = require("@typescript-eslint/parser");
+const react = require("eslint-plugin-react");
+const reactHooks = require("eslint-plugin-react-hooks");
 
-export default [
+module.exports = [
   js.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
